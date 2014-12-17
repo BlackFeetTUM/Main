d9c227e4749920747376406bd80856aadb733abb
Daniel <Felsy_Waschbaer@web.de>
2014-12-17 11:36:13 +0100

    Css fix for mobile

--- www/DashboardDozent.php
+++ www/DashboardDozent.php
@@ -10,7 +10,7 @@ and open the template in the editor.
         <title>TODO supply a title</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="dashboard.css" media="screen, projection" rel="stylesheet" type="text/css" />
+        <link href="dashboard.css" rel="stylesheet" type="text/css" />
         <script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
         <script src="Container.js"></script>
         <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
